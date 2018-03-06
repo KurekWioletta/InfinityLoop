71b0ebaaa310727e1dc372862ab27a28ac07bfac
Heecatee <heecatee67@gmail.com>
2018-03-06 19:05:17 +0100

    adding file structure

--- app/src/main/java/com/hecate/infinityloop/view/MenuActivity.java
+++ app/src/main/java/com/hecate/infinityloop/view/MenuActivity.java
@@ -1,13 +1,15 @@
-package com.hecate.infinityloop;
+package com.hecate.infinityloop.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.hecate.infinityloop.R;
+
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
     }
 }
